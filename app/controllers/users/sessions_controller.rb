cd04f29c9e5e2e99b80875521516a15ccf40652d
Jonathan de Oliveira Silva <jonathan.silva.impulso@yapay.com.br>
2022-06-11 20:37:47 -0300

    configure devise

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  private
+
+  def respond_with(_, _opts = {})
+    render json: { message: 'You are logged in.' }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    log_out_success && return if current_user
+
+    logout_out_failure
+  end
+
+  def log_out_success
+    render json: { message: 'You are logged out.' }, status: :ok
+  end
+
+  def logout_out_failure
+    render json: { message: 'Failure to logout'}, status: :unauthorized
+  end
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
